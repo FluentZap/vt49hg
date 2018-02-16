22f7f7e497578e2c3bb8ba5746add3e0683fb6a2
Toad <Toad@localhost>
2018-02-16 04:15:35 -0800

    Added Serial

---
+++ VT49/VTNetwork.cpp
@@ -0,0 +1,25 @@
+#include "VTNetwork.h"
+#include "SDL.h"
+#include "SDL_net.h"
+
+
+VTNetwork::VTNetwork()
+{
+}
+
+VTNetwork::~VTNetwork()
+{	
+//	SDLNet_Quit();
+}
+
+
+
+void VTNetwork::init_Network()
+{
+	/*
+	SDLNet_Init();
+	
+	SDLNet_ResolveHost( &ip, NULL, 4949 );	
+	tcpsock = SDLNet_TCP_Open( &ip );
+	 */
+}
