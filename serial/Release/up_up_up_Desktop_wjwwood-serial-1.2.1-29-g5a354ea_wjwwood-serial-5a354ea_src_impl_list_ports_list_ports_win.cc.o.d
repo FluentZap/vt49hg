2c3b3c5b02ef8c6b952bf628b3bc6b1134a8a431
Toad <Toad@localhost>
2018-02-20 10:19:33 -0800

    Got Serial WORKING!

---
+++ serial/Release/up_up_up_Desktop_wjwwood-serial-1.2.1-29-g5a354ea_wjwwood-serial-5a354ea_src_impl_list_ports_list_ports_win.cc.o.d
@@ -0,0 +1,8 @@
+Release/up_up_up_Desktop_wjwwood-serial-1.2.1-29-g5a354ea_wjwwood-serial-5a354ea_src_impl_list_ports_list_ports_win.cc.o: \
+ ../../../Desktop/wjwwood-serial-1.2.1-29-g5a354ea/wjwwood-serial-5a354ea/src/impl/list_ports/list_ports_win.cc \
+ C:\Users\Toad\Documents\VT49\VT49/serial/serial.h \
+ C:\Users\Toad\Documents\VT49\VT49/serial/v8stdint.h
+
+C:\Users\Toad\Documents\VT49\VT49/serial/serial.h:
+
+C:\Users\Toad\Documents\VT49\VT49/serial/v8stdint.h:
---
+++ serial/Release/up_up_up_Desktop_wjwwood-serial-1.2.1-29-g5a354ea_wjwwood-serial-5a354ea_src_impl_win.cc.o.d
@@ -0,0 +1,11 @@
+Release/up_up_up_Desktop_wjwwood-serial-1.2.1-29-g5a354ea_wjwwood-serial-5a354ea_src_impl_win.cc.o: \
+ ../../../Desktop/wjwwood-serial-1.2.1-29-g5a354ea/wjwwood-serial-5a354ea/src/impl/win.cc \
+ C:\Users\Toad\Documents\VT49\VT49/serial/impl/win.h \
+ C:\Users\Toad\Documents\VT49\VT49/serial/serial.h \
+ C:\Users\Toad\Documents\VT49\VT49/serial/v8stdint.h
+
+C:\Users\Toad\Documents\VT49\VT49/serial/impl/win.h:
+
+C:\Users\Toad\Documents\VT49\VT49/serial/serial.h:
+
+C:\Users\Toad\Documents\VT49\VT49/serial/v8stdint.h:
